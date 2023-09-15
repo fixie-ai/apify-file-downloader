19bbf28ccc3983bd6053e0911f6a76600b692e69
Matt Welsh <matt@fixie.ai>
2023-09-14 22:07:35 -0700

    Fix.

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 const Apify = require('apify');
 const got = require("got");
 
-// Helper functions
-const { log, dir, error } = console;
-
 Apify.main(async () => {
     const { url } = await Apify.getValue('INPUT');
-    log('Downloading URL: ', url);
-
+    console.log('Downloading URL: ', url);
     try {
       const rawData = await got(url).buffer();
       const b64Data = rawData.toString("base64");
-      log.info(`Successfully downloaded ${url}: ${rawData.length} bytes`);
+      console.log(`Successfully downloaded ${url}: ${rawData.length} bytes`);
       await Actor.pushData({ url: input.url, data: b64Data });
-
     } catch (error) {
       console.error(`There was a problem with the fetch operation for ${url}: ${error}`);
       return null;
