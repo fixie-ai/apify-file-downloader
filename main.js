1099101ba07871dbd1fb53a111f505c8ccb60798
Matt Welsh <matt@fixie.ai>
2023-09-14 22:16:55 -0700

    Use Apify SDK v3.

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const Apify = require('apify');
-const got = require("got");
+import { Actor } from "apify";
+import got from "got";
 
-Apify.main(async () => {
-    const { url } = await Apify.getValue('INPUT');
-    console.log('Downloading URL: ', url);
-    try {
-      const rawData = await got(url).buffer();
-      const b64Data = rawData.toString("base64");
-      console.log(`Successfully downloaded ${url}: ${rawData.length} bytes`);
-      await Actor.pushData({ url: input.url, data: b64Data });
-    } catch (error) {
-      console.error(`There was a problem with the fetch operation for ${url}: ${error}`);
-      return null;
-    }
-});
+await Actor.init();
 
+const { url } = await Actor.getInput();
+
+try {
+  const rawData = await got(url).buffer();
+  const b64Data = rawData.toString("base64");
+  console.log(`Successfully downloaded ${url}: ${rawData.length} bytes`);
+  await Actor.pushData({ url: input.url, data: b64Data });
+} catch (error) {
+  console.error(
+    `There was a problem with the fetch operation for ${url}: ${error}`
+  );
+}
+
+await Actor.exit();
