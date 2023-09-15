ca867f4517bfa72c0d5ca22f7670249fc98ca324
Matt Welsh <matt@fixie.ai>
2023-09-14 22:23:01 -0700

    Add more metadata.

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ await Actor.init();
 const { url } = await Actor.getInput();
 
 try {
-  const rawData = await got(url).buffer();
+  const response = await got(url);
+  const rawData = response.buffer();
   const b64Data = rawData.toString("base64");
   console.log(`Successfully downloaded ${url}: ${rawData.length} bytes`);
-  await Actor.pushData({ url: url, data: b64Data });
+  await Actor.pushData({
+    url: url,
+    mimeType: response.headers["content-type"],
+    data: b64Data,
+    dataLength: rawData.length,
+    timestamp: new Date().toISOString(),
+  });
 } catch (error) {
   console.error(
     `There was a problem with the fetch operation for ${url}: ${error}`
