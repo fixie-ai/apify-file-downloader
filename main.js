591dcede79be8bc98d8f13871ba053eec0005465
Matt Welsh <matt@fixie.ai>
2023-09-14 22:18:37 -0700

    Fix.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ try {
   const rawData = await got(url).buffer();
   const b64Data = rawData.toString("base64");
   console.log(`Successfully downloaded ${url}: ${rawData.length} bytes`);
-  await Actor.pushData({ url: input.url, data: b64Data });
+  await Actor.pushData({ url: url, data: b64Data });
 } catch (error) {
   console.error(
     `There was a problem with the fetch operation for ${url}: ${error}`
